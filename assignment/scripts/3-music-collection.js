cbe2c2b38f30e14c5ed36be3a3897fc175488cf4
LoElyse <lora.elyse14@gmail.com>
2024-03-22 17:28:12 -0500

    Creating objects

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,78 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+const myCollection = [];
 
+console.log('My array:', myCollection);
+
+function addToCollection( collection, artist, title, yearPublished) {
+
+
+  let album1 =  {
+    artist: artist,
+    title: title,
+    yearPublished: yearPublished
+  } 
+collection.push(album1);
+return album1;
+
+
+  let album2 = {
+    artist: artist,
+    title: title,
+    yearPublished: yearPublished
+   }
+   collection.push(album2);
+  return album2;
+  
+  let album3 = {
+    artist: artist,
+    title: title,
+    yearPublished: yearPublished
+  }
+  collection.push(album3);
+  return album3;
+
+  let album4 = {
+    artist: artist,
+    title: title,
+    yearPublished: yearPublished
+  }
+  collection.push(album4);
+  return album4;
+
+  let album5 = {
+    artist: artist,
+    title: title,
+    yearPublished: yearPublished
+  }
+  collection.push(album5);
+  return album5;
+ 
+  let album6 = {
+    artist: artist,
+    title: title,
+    yearPublished: yearPublished
+  }
+  collection.push(album6);
+  return album6;
+ 
+ }
+
+addToCollection(myCollection, 'Frank Ocean', 'Blonde', 2016)
+addToCollection(myCollection, 'SZA', 'SOS', 2022)
+addToCollection(myCollection, 'Fred again..', 'Actual Life 3', 2022)
+addToCollection(myCollection, 'Daft Punk', 'Random Access Memories', 2013)
+addToCollection(myCollection, 'Rufus Du Sol', 'Bloom', 2016)
+addToCollection(myCollection, 'Tame Impala', 'Currents', 2015)
+
+console.log('my collection:', myCollection)
+
+function showCollection(collection) {
+  for (i = 0; i < collection.length; i++) {
+    console.log('albums', collection);
+ }
+}
 
 
 
